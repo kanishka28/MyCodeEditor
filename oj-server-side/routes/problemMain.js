6671a0cbd7bb1ddb2cca1bd0c835ec737bf8bf3c
Kanishka <88936750+kanishka28@users.noreply.github.com>
2025-08-19 13:26:18 +0530

    Update problemMain.js

--- oj-server-side/routes/problemMain.js
+++ oj-server-side/routes/problemMain.js
@@ -1,7 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const exploreinproblem = require("./exploreinproblem");
 
-router.use("/problems", exploreinproblem);
- 
-module.exports = router;
