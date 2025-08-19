12a675a7bc0d283e8abd88af2e6dcb038c22915e
Kanishka <88936750+kanishka28@users.noreply.github.com>
2025-08-19 13:24:16 +0530

    Update middleware.js

--- oj-server-side/middleware.js
+++ oj-server-side/middleware.js
@@ -1,13 +1,1 @@
-const middlewareValidator = (req, res, next) => {
-  next();
-};
 
-const loginValidator = (req, res, next) => {
-  next();
-};
-
-const registerValidator = (req, res, next) => {
-  next();
-};
-
-module.exports = { middlewareValidator, loginValidator, registerValidator };
